682942cc66f9a0c60728c4a8efd99f264c82afcc
Vinod Nair <vinod.nair@relevancelab.com>
2016-07-12 12:32:38 +0530

    Included jenkins into startup

--- startup.sh
+++ startup.sh
@@ -4,12 +4,17 @@ mvn clean install -DskipTests
 cd api
 echo $1
 echo $2
-
+echo "Installing Nginx"
+apt-get install nginx -y
+echo "Stopping all java services"
+killall java
+echo "Configuring API"
 cp -f dashboard.template target/dashboard.properties
 echo "dbhost="$2 >> target/dashboard.properties
 cd target
 java -jar api.jar --spring.config.location=./dashboard.properties > /dev/null 2>&1 &
 
+echo "Configuring Jira collector"
 cd ../../jira-feature-collector/
 cp -f jira.template target/application.properties
 wget $1/d4dMastersCICD/readmasterjsonnew/23 -O target/temp.properties
@@ -18,6 +23,7 @@ echo "dbhost="$2 >> target/application.properties
 cd target
 java -jar jira-feature-collector.jar  > /dev/null 2>&1 &
 
+echo "Configuring Octopus collector"
 cd ../../octopus-deployment-collector/
 cp -f octopus.template target/application.properties
 wget $1/d4dMastersCICD/readmasterjsonnew/28 -O target/temp.properties
@@ -26,8 +32,18 @@ echo "dbhost="$2 >> target/application.properties
 cd target
 java -jar octopus-deployment-collector-2.0.2-SNAPSHOT.jar > /dev/null 2>&1 &
 
+echo "Configuring Jenkins collector"
+cd ../../jenkins-build-collector/
+cp -f jenkins.template target/application.properties
+wget $1/d4dMastersCICD/readmasterjsonnew/20 -O target/temp.properties
+cat target/temp.properties >> target/application.properties
+echo "dbhost="$2 >> target/application.properties
+cd target
+java -jar jenkins-build-collector-2.0.2-SNAPSHOT.jar > /dev/null 2>&1 &
 
+echo "Starting UI"
 cd ../../UI
 cp -r dist/* /usr/share/nginx/html/
 cat ../nginx.default > /etc/nginx/sites-enabled/default
 service nginx reload
+echo "Done..."
