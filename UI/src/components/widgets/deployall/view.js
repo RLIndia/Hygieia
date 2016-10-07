2394a76d13a64c0c5c7b02bd464540460bafc12c
getting2vinod <speed@123>
2016-10-07 13:47:34 +0530

    Started with Octopus All projects collector update and UI. Collector, api and New UI template created. Data preparation for UI done. Table view done. Multi env UI issue fixed. further Testing pending

--- UI/src/components/widgets/deployall/view.js
+++ UI/src/components/widgets/deployall/view.js
@@ -40,7 +40,7 @@
 
             for(var i = 0; i < data.length; i++){
 
-                if(data[i].environmentName == "Test14" || data[i].environmentName == "Load") //to be removed
+                if(data[i].environmentName == "Test14" || data[i].environmentName == "Load") {//to be removed
                     var machingIndx = -1;
                     for(var j =0; j < masterenv.length;j++){
                         if(masterenv[j].environmentID == data[i].environmentID){
