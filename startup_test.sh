c66000f1d748c0be91c65939ce3897a4ec34c30c
Vinod Nair <getting2vinod@gmail.com>
2017-03-01 10:51:25 +0530

    Testing startup script

--- startup_test.sh
+++ startup_test.sh
@@ -12,9 +12,9 @@ cd api/target
 
 echo "Configuring Chef collector"
 cd ../../chef-collector/
-cp -f jenkins.template target/application.properties
-wget --header="Accept-Charset: UTF-8"  --header="x-catalyst-auth:\"$token\"" $1/d4dMastersCICD/readmasterjsonnew/20 -O target/temp.properties
+cp -f chef.template target/application.properties
+wget --header="Accept-Charset: UTF-8"  --header="x-catalyst-auth:\"$token\"" $1/d4dMastersCICD/readmasterjsonnew/10 -O target/temp.properties
 cat target/temp.properties >> target/application.properties
 echo "dbhost="$2 >> target/application.properties
 cd target
-nohup java -jar jenkins-build-collector-2.0.2-SNAPSHOT.jar &
+nohup java -jar chef-collector-2.0.2-SNAPSHOT.jar &
